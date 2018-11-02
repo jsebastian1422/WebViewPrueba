2fe0f7227954e7239ee02387192522730720c3a0
jsebastian1422 <jsebastian1422@gmail.com>
2018-11-02 15:10:40 -0500

    Inicializando App

---
+++ app/src/main/java/com/example/sistemas/webview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sistemas.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://developer.chrome.com/multidevice/webview/gettingstarted");
+    }
+}
